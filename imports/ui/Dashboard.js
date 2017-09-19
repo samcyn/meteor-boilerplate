951985d608d6ad019562ab779c06a0a0369e4cd3
samcyn <samsoniyanda@outlook.com>
2017-09-20 00:58:29 +0100

    Init commit

---
+++ imports/ui/Dashboard.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import PrivateHeader from './PrivateHeader';
+
+
+
+const Dashboard = () => {
+  return (
+    <div>
+     
+      <PrivateHeader title = "Dashboard"/>
+      
+      <div className="page-content">
+
+        
+
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
